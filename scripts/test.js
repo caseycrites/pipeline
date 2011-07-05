24fdcbfc9fec21e8a30e75bd7996a94ad1a16480
Casey W Crites <crites.casey@gmail.com>
2011-07-05 11:33:46 -0700

    Added a Pipeline object and a way to deal with them.  Also a test script.

---
+++ scripts/test.js
@@ -0,0 +1,16 @@
+var settings = require('pipeline/settings').autoload()
+  , pipelines = require('pipelines')(settings)
+  , User = require('storage/User')
+  , penelope = require('sgutil/penelope')(settings)
+
+var pipeline = new Pipeline({
+  name: 'test.pipeline',
+  owner: 2270,
+  'public': true,
+  body: {this: 'is', sample: 'pipeline'}
+})
+
+penelope.save('Pipelines', pipeline.name, pipeline.toCassandra(), penelope.makeTimestamp(), function(err) {
+  if (err) throw err
+  console.log('created pipeline: ', JSON.stringify(pipeline))
+}
