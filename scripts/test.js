b37e4c253cb1b5095d106f87e4e3a7686d72788c
Casey W Crites <crites.casey@gmail.com>
2011-07-05 14:33:05 -0700

    Modified the script to actually work.

--- scripts/test.js
+++ scripts/test.js
@@ -2,16 +2,23 @@
 // vim: filetype=javascript
 
 var settings = require('pipeline/settings').autoload()
+  , Pipeline = require('pipeline/Pipeline')
   , penelope = require('sgutil').penelope(settings)
 
 var pipeline = new Pipeline({
   name: 'test.pipeline',
   owner: 2270,
   'public': true,
-  body: {this: 'is', sample: 'pipeline'}
+  body: {this: 'is', sample: 'pipeline'},
+  created: penelope.makeTimestamp()
 })
 
 penelope.save('Pipelines', pipeline.name, pipeline.toCassandra(), penelope.makeTimestamp(), function(err) {
   if (err) throw err
   console.log('created pipeline: ', JSON.stringify(pipeline))
-}
+  penelope.load('Pipelines', pipeline.name, function(err, data) {
+    if (err) throw err
+    console.log('retrieved pipeline: ', data)
+    process.exit(0)
+  })
+})
